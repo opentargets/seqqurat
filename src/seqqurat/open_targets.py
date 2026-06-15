e7c4e2bc701f2904ab42fffadd7721ca5129701d
project-defiant <szymonszyszkowski@gmail.com>
2026-06-15 23:26:32 +0100

    feat(26.06): schema update

--- src/seqqurat/open_targets.py
+++ src/seqqurat/open_targets.py
@@ -43,6 +43,7 @@ class OpenTargetsDatasetSchemaRegistry:
         '25.12-ppp': BASE_SCHEMA_PATH / '25.12-ppp' / 'release.yaml',
         '26.03': BASE_SCHEMA_PATH / '26.03' / 'release.yaml',
         '26.03-ppp': BASE_SCHEMA_PATH / '26.03-ppp' / 'release.yaml',
+        '26.06': BASE_SCHEMA_PATH / '26.06' / 'release.yaml',
     }
 
     SCHEMA = {r: OpenTargetsDatasetModel.from_path(p) for r, p in SCHEMA_PATHS.items()}
